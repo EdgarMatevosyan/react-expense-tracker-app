445d91b1d0d6812a4026bbfe66b238e103329db7
Edgar Matevosyan <edgarmatevosyan@gmail.com>
2021-03-26 18:26:05 -0700

    all components and styling added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import "./App.css";
 import React from "react";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransactions } from "./components/AddTransactions";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransactions />
+      </div>
+    </div>
+  );
 }
 
 export default App;
