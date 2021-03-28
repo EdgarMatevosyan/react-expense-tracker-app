0655c7b0b5c11f34232b50d526f5ad100c73adbe
Edgar Matevosyan <edgarmatevosyan@gmail.com>
2021-03-28 13:21:27 -0700

    added delete transaction, balance information and history ui

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,13 @@
 export default (state, action) => {
   switch (action.type) {
+    // A Reducer is just a way to change the state and send it down to your component
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
     default:
       return state;
   }
